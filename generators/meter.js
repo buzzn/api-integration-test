8047b4090af274730fbe43fb43be26af3bb9aaaf
ChipJuggler <scsi.rdx@gmail.com>
2019-05-08 18:25:52 +0200

    es6 magic

--- generators/meter.js
+++ generators/meter.js
@@ -9,52 +9,48 @@ const generateMeteringPointId = generateHardwareId({ length: 33 });
 
 const generateMarketLocationId = generateHardwareId({ length: 11 });
 
-function generateGridMeter() {
-  let data = generateMeter(true);
-  data['registers'] = [
+const generateGridMeter = () => ({
+  ...generateMeter(true),
+  registers: [
     {
       label: 'GRID_CONSUMPTION',
       name: 'grid',
-      market_location_id: generateMarketLocationId()
+      market_location_id: generateMarketLocationId(),
     },
     {
       label: 'GRID_FEEDING',
       name: 'grid',
-      market_location_id: generateMarketLocationId()
-    }
-  ];
-  return data;
-}
+      market_location_id: generateMarketLocationId(),
+    },
+  ],
+});
 
-function generateMeter(one_way) {
-  let data = {
-    type: 'real',
-    datasource: 'standard_profile',
-    manufacturer_name: 'other',
-    product_serialnumber: chance.string(),
-    product_name: 'ExampleMeter',
-    direction_number: one_way ? 'ERZ' : 'ZRZ',
-    calibrated_until : '2027-10-13',
-    converter_constant: 1,
-    ownership: 'BUZZN',
-    build_year: 2008,
-    sent_data_dso: '2009-01-01',
-    edifact_metering_type: 'EHZ',
-    edifact_meter_size: 'Z01',
-    edifact_measurement_method: 'MMR',
-    edifact_mounting_method: 'DPA',
-    edifact_voltage_level: 'E06',
-    edifact_cycle_interval: 'YEARLY',
-    edifact_tariff: 'ETZ',
-    edifact_data_logging: 'Z05',
-    metering_location_id: generateMeteringPointId()
-  };
-  return data;
-}
+const generateMeter = one_way => ({
+  type: 'real',
+  datasource: 'standard_profile',
+  manufacturer_name: 'other',
+  product_serialnumber: chance.string(),
+  product_name: 'ExampleMeter',
+  direction_number: one_way ? 'ERZ' : 'ZRZ',
+  calibrated_until: '2027-10-13',
+  converter_constant: 1,
+  ownership: 'BUZZN',
+  build_year: 2008,
+  sent_data_dso: '2009-01-01',
+  edifact_metering_type: 'EHZ',
+  edifact_meter_size: 'Z01',
+  edifact_measurement_method: 'MMR',
+  edifact_mounting_method: 'DPA',
+  edifact_voltage_level: 'E06',
+  edifact_cycle_interval: 'YEARLY',
+  edifact_tariff: 'ETZ',
+  edifact_data_logging: 'Z05',
+  metering_location_id: generateMeteringPointId(),
+});
 
 module.exports = {
   generateMeteringPointId,
   generateMarketLocationId,
   generateMeter,
-  generateGridMeter
+  generateGridMeter,
 };
